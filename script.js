376ca7f5bc5008e6904760260d45e71daeef841d
Richard Davis <rdavis.bacs@gmail.com>
2021-04-19 15:24:18 -0400

    add form, print to console when button is pressed

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function main() {
-  console.log("DOM has loaded...");
+const main = () => {
+  const form = document.getElementById("short-url-form");
+  form.addEventListener("submit", event => {
+    event.preventDefault();
+    console.log('button pressed');
+  });
 }
+
 document.addEventListener("DOMContentLoaded", main);
