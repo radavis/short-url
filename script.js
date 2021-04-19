52e4195888889580fbfcd1a799c3873a2a32700a
Richard Davis <rdavis.bacs@gmail.com>
2021-04-19 16:46:38 -0400

    working

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-const main = () => {
-  const form = document.getElementById("short-url-form");
-  form.addEventListener("submit", event => {
-    event.preventDefault();
-    console.log('button pressed');
-  });
-}
+const BITLY_API_ACCESS_TOKEN = "your-access-token-here";
+// note: I was unable to create a bitly access token with a social media login.
+// Create a bitly account with an email and password.
 
-document.addEventListener("DOMContentLoaded", main);
+const form = document.getElementById("form");
+const urlField = document.getElementById("url");
+const result = document.getElementById("result");
+const debug = document.getElementById("debug");
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  // https://dev.bitly.com/api-reference
+  const data = { long_url: urlField.value };
+  fetch("https://api-ssl.bitly.com/v4/shorten", {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${BITLY_API_ACCESS_TOKEN}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    return response.json();
+  })
+  .then(json => {
+    console.log(json);
+    result.innerHTML = json.link;
+    debug.innerHTML = JSON.stringify(json, null, 2);
+  })
+  .catch(err => debug.innerHTML = err);
+});
